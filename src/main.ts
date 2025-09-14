dadd60d6160acc8303d78bd176848d9d3a3a8a69
sanjeevkumar007 <san.ank007@gmail.com>
2025-09-15 00:13:01 +0530

    changed background color

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,11 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { jwtInterceptor } from './app/_interceptors/jwt.interceptor';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 bootstrapApplication(AppComponent, {
   providers: [
+    provideAnimations(),
     provideRouter(routes),
     provideHttpClient(
       withInterceptors([jwtInterceptor])
